9060b3c7963965d6a493985d75319a09a0b93664
ab1428x <ab1428x@gmail.com>
2025-07-09 14:21:55 -0400

    Initial Commit

---
+++ iShuffMan/iShuffManApp.swift
@@ -0,0 +1,17 @@
+//
+//  iShuffManApp.swift
+//  iShuffMan
+//
+//  Created by Jay on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct iShuffManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
